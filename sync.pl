d718d0949de411e2638f5517c0ed87c9fba1b63b
Marcin Radlak <mar.radlak@gmail.com>
2014-01-10 19:53:52 +0100

    Headers.

---
+++ sync.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+
+#################################################################
+#																#
+#	FTP Synchronizer											#
+#	Projekt zaliczeniowy z Pracowni Jezykow Skryptowych			#
+#	Uniwersytet Jagiellonski, Krakow							#
+#																#
+#	Automatyczna synchronizacja zmienionego pliku na serwer		#
+#	(c) 2014 Marcin Radlak										#
+#	marcin.radlak@uj.edu.pl 									#
+#	http://marcinradlak.pl 										#
+#																#
+#################################################################
